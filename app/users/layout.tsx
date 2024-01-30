fdabe2a634f40ff83d02ab2b716e2c1cb93ddc63
Vignesh <vigneshpamu2002@gmail.com>
2024-01-30 16:02:14 +0530

    sadfsdf

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,24 +1,21 @@
-// import getUsers from '../actions/getUsers'
-// import Sidebar from '../components/sidebar/Sidebar'
-// import UserList from './components/UserList'
-
+import getUsers from '../actions/getUsers'
 import Sidebar from '../components/sidebar/Sidebar'
+import UserList from './components/UserList'
 
 export default async function UsersLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-  //   const users = await getUsers()
+  const users = await getUsers()
 
   return (
     // @ts-expect-error Server Component
     <Sidebar>
       <div className="h-full">
-        {/* <UserList items={users} /> */}
+        <UserList items={users} />
         {children}
       </div>
-      //{' '}
     </Sidebar>
   )
 }
