ce63d4d7f85ae279dbbb54be695affce768d1748
Vignesh <vigneshpamu2002@gmail.com>
2024-01-31 02:13:31 +0530

    dsfsdsd

--- app/actions/getSession.ts
+++ app/actions/getSession.ts
@@ -1,6 +1,7 @@
 import { getServerSession } from 'next-auth'
+import authOptions from './authOptions'
 
-import { authOptions } from '@/app/api/auth/[...nextauth]/route'
+// import { authOptions } from '@/app/api/auth/[...nextauth]/route'
 
 export default async function getSession() {
   return await getServerSession(authOptions)
