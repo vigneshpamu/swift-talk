bc06a29e4c9e3a5cecd0876ccccb248961da97cb
Vignesh <vigneshpamu2002@gmail.com>
2024-01-30 13:28:47 +0530

    dfsadfadf

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@
 const nextConfig = {
   experimental: {
     appDir: true,
+    swcPlugins: [['next-superjson-plugin', {}]],
+  },
+  images: {
+    domains: [
+      'res.cloudinary.com',
+      'avatars.githubusercontent.com',
+      'lh3.googleusercontent.com',
+    ],
   },
 }
 
